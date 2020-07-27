653624f204195c3edda9e30ecc36dbf5750ddca5
Ulrich Titz <u.titz@industry-analytics.com>
2020-07-27 16:07:25 +0200

    ci

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
-docker build -t ulrichjohannes/multi-client -f ./client/Dockerfile ./client
-docker build -t ulrichjohannes/multi-server -f ./server/Dockerfile ./server
-docker build -t ulrichjohannes/multi-worker -f ./worker/Dockerfile ./worker
-docker push ulrichjohannes/multi-client
-docker push ulrichjohannes/multi-server
-docker push ulrichjohannes/multi-worker
+docker build -t ulrichjohannes/multi-client:latest -t ulrichjohannes/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker build -t ulrichjohannes/multi-server -t ulrichjohannes/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ulrichjohannes/multi-worker -t ulrichjohannes/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ulrichjohannes/multi-client:latest
+docker push ulrichjohannes/multi-server:latest
+docker push ulrichjohannes/multi-worker:latest
+docker push ulrichjohannes/multi-client:$SHA
+docker push ulrichjohannes/multi-server:$SHA
+docker push ulrichjohannes/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ulrichjohannes/multi-server
+kubectl set image deployments/server-deployment server=ulrichjohannes/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=ulrichjohannes/multi-worker:$SHA
+kubectl set image deployments/client-deployment server=ulrichjohannes/multi-client:$SHA
