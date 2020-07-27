7c8052da5306f2bee6f6fe59f521843604733836
Ulrich Titz <u.titz@industry-analytics.com>
2020-07-27 15:31:26 +0200

    ci

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 docker build -t ulrichjohannes/multi-client -f ./client/Dockerfile ./client
 docker build -t ulrichjohannes/multi-server -f ./server/Dockerfile ./server
 docker build -t ulrichjohannes/multi-worker -f ./worker/Dockerfile ./worker
+docker push ulrichjohannes/multi-client
+docker push ulrichjohannes/multi-server
+docker push ulrichjohannes/multi-worker
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ulrichjohannes/multi-server
